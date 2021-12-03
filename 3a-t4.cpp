599d885d8167c0a1c646b05a579b98b25eb3da02
Inamaes <41383331+Inamaes@users.noreply.github.com>
2021-12-03 21:39:57 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,16 +1,17 @@
-#include<iostream> 
-using namespace std; 
-
+#include<iostream>
+using namespace std;
 int main()
 {
 int k, i = 1, sum_k = 0, number;
 cout << " Enter the amount of integers to sum = "; cin >> k;
 do {
-cout << "Enter integer nr. " << i << ": "; 
+if (k == 0) // if statement, so cycle will break if user inputs zero as the k value
+break;
+cout << "Enter integer nr. " << i << ": ";
 cin >> number;
-sum_k += number; 
+sum_k += number;
 i++;
 } while (i <= k);
-cout << " The total sum of " << k << " integers is: "  << sum_k;
+cout << "The total sum of " << k << " integers is: " << sum_k;
 return 0;
 }
